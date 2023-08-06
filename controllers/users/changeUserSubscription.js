33d6d63987555d3f839364185ec31f3b35f31090
Ivan Klimov <klimov128@gmail.com>
2023-08-06 23:20:24 +0300

    Create changeUserSubscription.js

---
+++ controllers/users/changeUserSubscription.js
@@ -0,0 +1,35 @@
+const User = require('../../models/userModel');
+
+exports.changeUserSubscription = async (req, res) => {
+  try {
+    const subscriptionType = ['starter', 'pro', 'business'];
+    const newSubscription = req.body.subscription;
+    const { email, subscription } = req.user;
+
+    const isSubscriptionValid = subscriptionType.some(
+      (item) => item === newSubscription
+    );
+
+    console.log('isSubscriptionValid', isSubscriptionValid);
+
+    if (!isSubscriptionValid) {
+      return res
+        .status(400)
+        .json({ message: `subscription "${newSubscription}" does not exist` });
+    }
+
+    if (newSubscription === subscription) {
+      return res.status(200).json({
+        message: `You already have a "${newSubscription}" subscription`,
+      });
+    }
+
+    await User.updateOne({ email }, { subscription: newSubscription });
+
+    res.status(200).json({
+      message: `Your subscription has been successfully updated to "${newSubscription}"`,
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
