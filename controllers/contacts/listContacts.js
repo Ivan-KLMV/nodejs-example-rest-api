60426354ed6abb883e51289ba9b5173588d528ec
Ivan Klimov <klimov128@gmail.com>
2023-08-06 23:20:17 +0300

    Update listContacts.js
    added search by favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,11 +2,18 @@ const Contact = require('../../models/contactModel');
 
 exports.listContacts = async (req, res) => {
   try {
-    const { _id: owner } = req.user;
-    const { page = 1, limit = 10 } = req.query;
+    const findParams = { owner: req.user.id };
+
+    const searchByFavorite = req.query.favorite;
+
+    if (searchByFavorite) {
+      findParams.favorite = searchByFavorite;
+    }
+
+    const { page = 1, limit = 5 } = req.query;
     const skip = (page - 1) * limit;
 
-    const contacts = await Contact.find({ owner }).skip(skip).limit(limit);
+    const contacts = await Contact.find(findParams).skip(skip).limit(limit);
 
     res.status(200).json(contacts);
   } catch (error) {
