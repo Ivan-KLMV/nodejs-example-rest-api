90211064af3141cd718474170d571e7a5e6f0695
Ivan Klimov <klimov128@gmail.com>
2023-08-06 21:02:41 +0300

    Update authProtect.js
    token cheking added

--- middleware/users/authProtect.js
+++ middleware/users/authProtect.js
@@ -7,8 +7,8 @@ exports.authProtect = async (req, res, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer') {
-    res.status(401).json({
+  if (bearer !== 'Bearer' && !token) {
+    return res.status(401).json({
       message: 'Not authorized',
     });
   }
