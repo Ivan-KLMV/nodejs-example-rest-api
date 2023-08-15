85a65b03c7b7c977f82811f99cb124354bddbdfc
Ivan Klimov <klimov128@gmail.com>
2023-08-15 17:37:39 +0300

    Update registerUser.js

--- controllers/users/registerUser.js
+++ controllers/users/registerUser.js
@@ -1,12 +1,23 @@
 const { nanoid } = require('nanoid');
 const User = require('../../models/userModel');
+const { emailSender } = require('../../utils');
 
 exports.registerUser = async (req, res) => {
   try {
     const verificationToken = nanoid();
-    console.log(verificationToken);
+    const { BASE_URL } = process.env;
+    const { email } = req.body;
     const newUser = await User.create({ ...req.body, verificationToken });
+
     newUser.password = undefined;
+
+    const verifyEmail = {
+      to: email,
+      subject: 'Verify email',
+      html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await emailSender(verifyEmail);
 
     res.status(201).json({
       user: { emal: newUser.email, subscription: newUser.subscription },
