4c735b11f5e988849e3e8f48ac48863e0b34ff04
Ivan Klimov <klimov128@gmail.com>
2023-08-17 12:04:05 +0300

    Update emailSender.js

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -2,14 +2,14 @@ const nodemailer = require('nodemailer');
 const { META_PASSWORD } = process.env;
 
 exports.emailSender = async (data) => {
-  const emailOptions = { ...data, from: 'ivan.klmv@meta.ua' };
+  const emailOptions = { ...data, from: 'mailsenderforstudy@meta.ua' };
 
   const config = {
     host: 'smtp.meta.ua',
     port: 465,
     secure: true,
     auth: {
-      user: 'ivan.klmv@meta.ua',
+      user: 'mailsenderforstudy@meta.ua',
       pass: META_PASSWORD,
     },
   };
